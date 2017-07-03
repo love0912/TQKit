cd9bc21dfc974b3628588f2a14980e613b67d9da
guojie <cqwan.gj@qq.com>
2017-07-03 11:15:52 +0800

    modify get networking date

--- TQKit/TQKit/Category/Foundation/NSDate/NSDate+TTExt2InternetDate.m
+++ TQKit/TQKit/Category/Foundation/NSDate/NSDate+TTExt2InternetDate.m
@@ -44,10 +44,24 @@ static NSDateFormatter *_internetDateTimeFormatter = nil;
     [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
     NSString *date = [[response allHeaderFields] objectForKey:@"Date"];
     NSDate* inputDate = [NSDate tt_dateFromInternetDateTimeString:date formatHint:TTDateFormatHintRFC822];
-    NSTimeZone *zone = [NSTimeZone systemTimeZone];
-    NSInteger interval = [zone secondsFromGMTForDate: inputDate];
-    NSDate *localeDate = [inputDate  dateByAddingTimeInterval: interval];
-    return localeDate;
+    //设置源日期时区
+    //    NSTimeZone* sourceTimeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];//或GMT
+    //    //设置转换后的目标日期时区
+    //    NSTimeZone* destinationTimeZone = [NSTimeZone localTimeZone];
+    //    //得到源日期与世界标准时间的偏移量
+    //    NSInteger sourceGMTOffset = [sourceTimeZone secondsFromGMTForDate:inputDate];
+    //    //目标日期与本地时区的偏移量
+    //    NSInteger destinationGMTOffset = [destinationTimeZone secondsFromGMTForDate:inputDate];
+    //    //得到时间偏移量的差值
+    //    NSTimeInterval interval = destinationGMTOffset - sourceGMTOffset;
+    //    //转为现在时间
+    //    NSDate* destinationDateNow = [[NSDate alloc] initWithTimeInterval:interval sinceDate:inputDate];
+    
+    
+    //    NSTimeZone *zone = [NSTimeZone systemTimeZone];
+    //    NSInteger interval = [zone secondsFromGMTForDate: inputDate];
+    //    NSDate *localeDate = [inputDate  dateByAddingTimeInterval: interval];
+    return inputDate;
 #pragma clang diagnostic pop
 }
 
